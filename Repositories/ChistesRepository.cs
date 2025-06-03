59df0bd5ced85a9decf44763bd8671356aeb2e07
Galo Guevara <galo.guevara.torres@udla.edu.ec>
2025-06-03 08:06:11 -0500

    feat: obtener chiste de la api

---
+++ Repositories/ChistesRepository.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace GuevaraGExamenProg2.Repositories
+{
+    internal class ChistesRepository
+    {
+        private readonly HttpClient _httpClient;
+        public ChistesRepository()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<string> ObtenerChistesAsync()
+        {
+            //obtener respuesta api
+            var response = await _httpClient.GetStringAsync("https://official-joke-api.appspot.com/random_joke");   
+            //abrir paquete json
+            var json = JObject.Parse(response);
+            //obtener variables
+            string setup = json["setup"]?.ToString();
+            string punchline = json["punchline"]?.ToString();
+            
+            return $"{setup}\n{punchline}";
+        }
+
+    }
+
+}
